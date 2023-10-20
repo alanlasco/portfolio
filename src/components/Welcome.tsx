f35c115fe84363de76f88ac961214e3b1ba1bf84
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-10-20 20:16:27 -0300

    primer paso, bienvenida

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../stylesheets/welcome.css';
+
+export const Welcome = () => {
+  return (
+    <>
+    <div className='welcome-container'>
+        <div className='foto'>
+          <div className='texto-container'>
+          <span className='texto'>Hola mundo</span>
+          </div>
+        </div>
+    </div>    
+    </>
+  )
+}
