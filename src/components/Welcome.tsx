b2701dd0621f1145c8a5d49b3737c8fcf9fa5672
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-10-22 00:15:49 -0300

    agregar estilos a inicio, boton

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -7,15 +7,21 @@ export const Welcome = () => {
     <div className='welcome-container'>
         <div className='foto'>
           <div className='texto-container'>
-          <span className='texto'>{'> '}¡Hola, Mundo!</span>
+          <p className='texto'>{'> '}¡Hola, Mundo!</p>
+          <p className='nombre'>Soy Alan Lasco.</p>
+          <p className='cargo'>Desarrollador web.</p>
             {/* boton */}
-            <button className="learn-more">
+            {/* <button className="learn-more"> *
               <span className="circle" aria-hidden="true">
                 <span className="icon arrow"></span>
               </span>
               <span className="button-text">Proyectos</span>
-            </button> 
-
+             </button>  */}
+            <p className='frase'>Front End developer; creando páginas web <span className='texto_resaltado'>atractivas</span> e <span className='texto_resaltado'>intuitivas</span> para el usuario.</p>
+            <button className="button" data-text="Awesome">
+              <span className="actual-text">&nbsp;Proyectos.&nbsp;</span>
+              <span aria-hidden="true" className="hover-text">&nbsp;Proyectos.&nbsp;</span>
+            </button>
 
           </div>
 
