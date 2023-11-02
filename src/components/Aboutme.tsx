ccaa135411183b8485b91c1827be3b2a5463cbbc
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-11-02 18:09:38 -0300

    cambiar sobre mi, cambiar tamaños de iconos

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -11,12 +11,9 @@ export const Aboutme = () => {
     <div id='sobremi' className='sobremi-container'>
               <p className='sobre_mi_titulo'><span className='flecha-2'>&gt;</span><span className='sobre'> Sobre </span><span className='texto_resaltado_2'>mi.</span></p> 
       <p className='sobre_mi_texto'>
-        ¡Hola! Soy Alan; desarrollador Front End React. Aunque me considero un programador versátil, gracias a las bases sólidas que me dio estudiar en la Facultad de Informática de La Plata, tanto en lenguajes de bajo nivel, como de alto nivel; lo cual me brinda otra perspectiva y la posibilidad de abarcar distintas tecnologías y áreas del mundo I.T.
-
-         Mi incursión en el desarrollo web, se dio gracias a diversos cursos extracurriculares que realizó la misma facultad, los cuales me incentivaron a seguir aprendiendo por mi cuenta.
-        
+        ¡Hola! Soy Alan; desarrollador Front End React. Aunque me considero un programador versátil, con gran adaptabilidad a diferentes tecnologías y areas del desarrollo        
        </p>
-        <p className='sobre_mi_texto tecnologias-presentacion'>Estas son las tecnologías que he manejado:</p> 
+        <p className='sobre_mi_texto tecnologias-presentacion'>Estas son las tecnologías que he estado utilizando:</p> 
         <div className='tecnologias'>
         <div className='icon-container'>
             <img className='icon' src={require('../icons/html-icon.png')} alt="" />
@@ -70,7 +67,8 @@ export const Aboutme = () => {
 
         </div>
         <p className='sobre_mi_titulo_2'>¿Un poco más sobre <span className='texto_resaltado_2'>mí</span>?</p>
-              <p className='sobre_mi_texto fin'>Soy aficionado a la poesía; Estudio antropología, la cual considero un factor importante para el análisis en la relación que tiene el ser humano con el mundo digital. Actualmente, me encuentro estudiando 6to año de inglés en un instituto. También reparo celulares y computadoras.  </p>
+              <p className='sobre_mi_texto fin'> Mi incursión en el desarrollo web, se dió gracias a diversos cursos extracurriculares que realicé en la Facultad de Informática de La Plata, los cuales me incentivaron a seguir aprendiendo por mi cuenta.
+ Soy aficionado a la poesía; Estudio antropología, la cual considero un factor importante para el análisis en la relación que tiene el ser humano con el mundo digital. Actualmente, me encuentro estudiando 6to año de inglés en un instituto. También reparo celulares y computadoras.  </p>
     </div> 
     </>
   )
