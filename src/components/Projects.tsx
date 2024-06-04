b015586d5e1fb48e26f6c902fcb786272ce4ca57
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-06-04 16:57:22 -0300

    responsive fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,58 +11,69 @@ export const Projects = () => {
           <span className="flecha-2">&gt;</span>
           <span className="sobre">{t("titulo")}</span>
         </p>
+        <div className="allProjects">
+          <Project
+            nombre={t("proyectoceronombre")}
+            tecnologias="HTML5, CSS, JavaScript"
+            imagen="assassinscreed.png"
+            descripcion={t("proyectoceroinfo")}
+            desplegar="link-icon"
+            github="https://github.com/alanlasco/rick-and-morty"
+            href="https://alanlasco.github.io/rick-and-morty/"
+            ocultar=""
+          />
+          <Project
+            nombre={t("proyectoceronombre")}
+            tecnologias="TypeScript, React, TailwindCSS"
+            imagen="rickymorty.png"
+            descripcion={t("proyectoceroinfo")}
+            desplegar="link-icon"
+            github="https://github.com/alanlasco/rick-and-morty"
+            href="https://alanlasco.github.io/rick-and-morty/"
+            ocultar=""
+          />
 
-        <Project
-          nombre={t("proyectoceronombre")}
-          tecnologias="TypeScript, React, TailwindCSS"
-          imagen="rickymorty.png"
-          descripcion={t("proyectoceroinfo")}
-          desplegar="link-icon"
-          github="https://github.com/alanlasco/rick-and-morty"
-          href="https://alanlasco.github.io/rick-and-morty/"
-          ocultar="ocultar"
-        />
-
-        <Project
-          nombre={t("proyectodosnombre")}
-          tecnologias="HTML5, CSS3, Bootstrap4, jQuery"
-          imagen="artist-img.jpeg"
-          descripcion={t("proyectodosinfo")}
-          desplegar="link-icon"
-          github="https://github.com/alanlasco/web-for-artists"
-          href="https://alanlasco.github.io/web-for-artists/"
-          ocultar=""
-        />
-        <Project
-          nombre={t("proyectotresnombre")}
-          tecnologias="TypeScript, React, CSS3"
-          imagen="alanpoetry-img.jpeg"
-          descripcion={t("proyectotresinfo")}
-          desplegar=""
-          github="https://github.com/alanlasco/alan-poetry"
-          href=""
-          ocultar="ocultar"
-        />
-        <Project
-          nombre={t("proyectocuatronombre")}
-          tecnologias="HTML5, PHP, SQL"
-          imagen="crud-img.jpeg"
-          descripcion={t("proyectocuatroinfo")}
-          desplegar=""
-          github="https://github.com/alanlasco/crud-alan-poetry-php"
-          href=""
-          ocultar="ocultar"
-        />
-        <Project
-          nombre={t("proyectounonombre")}
-          tecnologias="TypeScript, React, CSS3"
-          imagen="portfolio-img.jpeg"
-          descripcion={t("proyectounoinfo")}
-          desplegar=""
-          github="https://github.com/alanlasco/portfolio"
-          href=""
-          ocultar="ocultar"
-        />
+          <Project
+            nombre={t("proyectodosnombre")}
+            tecnologias="HTML5, CSS3, Bootstrap4, jQuery"
+            imagen="artist-img.jpeg"
+            descripcion={t("proyectodosinfo")}
+            desplegar="link-icon"
+            github="https://github.com/alanlasco/web-for-artists"
+            href="https://alanlasco.github.io/web-for-artists/"
+            ocultar=""
+          />
+          <Project
+            nombre={t("proyectotresnombre")}
+            tecnologias="TypeScript, React, CSS3"
+            imagen="alanpoetry-img.jpeg"
+            descripcion={t("proyectotresinfo")}
+            desplegar=""
+            github="https://github.com/alanlasco/alan-poetry"
+            href=""
+            ocultar="ocultar"
+          />
+          <Project
+            nombre={t("proyectocuatronombre")}
+            tecnologias="HTML5, PHP, SQL"
+            imagen="crud-img.jpeg"
+            descripcion={t("proyectocuatroinfo")}
+            desplegar=""
+            github="https://github.com/alanlasco/crud-alan-poetry-php"
+            href=""
+            ocultar="ocultar"
+          />
+          <Project
+            nombre={t("proyectounonombre")}
+            tecnologias="TypeScript, React, CSS3"
+            imagen="portfolio-img.jpeg"
+            descripcion={t("proyectounoinfo")}
+            desplegar=""
+            github="https://github.com/alanlasco/portfolio"
+            href=""
+            ocultar="ocultar"
+          />
+        </div>
       </div>
     </>
   );
